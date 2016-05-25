aa5fd577e573573208151c5eab63f6e94ed71c38
mattschreiber <matt.schreiber01@gmail.com>
2016-05-25 19:50:06 -0400

    changed loop for todoitems seed file

--- mod2_homework/todolists/db/seeds.rb
+++ mod2_homework/todolists/db/seeds.rb
@@ -5,14 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-array_of_todoitems = [
-	{title: "Item1", description: "This is item 1"},
-	{title: "Item2", description: "This is item 2"},
-	{title: "Item3", description: "This is item 3"},
-	{title: "Item4", description: "This is item 4"},
-	{title: "Item5", description: "This is item 5"}
-]
+num_items = 5 #number of Todoitems to create
+todoitems = {title: "Item", description: "This is item #", due_date: Date.today+1.year}
+# [	{title: "Item2", description: "This is item 2"},
+# 	{title: "Item3", description: "This is item 3"},
+# 	{title: "Item4", description: "This is item 4"},
+# 	{title: "Item5", description: "This is item 5"}
+# ]
 
 User.destroy_all
 TodoList.destroy_all
@@ -20,30 +19,38 @@ TodoList.destroy_all
 p = User.create!({username: "Fiorina", password_digest: "1234"})
 p.create_profile!({first_name: "Carly", last_name: "Fiorina", birth_year: 1954, gender: "female"})
 l = TodoList.create!(list_name: "test", list_due_date: Date.today+1.year, user_id: "#{p['id']}")
-array_of_todoitems.each do |item|
- 	l.todo_items.create(title: item[:title], description: item[:description], due_date: Date.today+1.year, todo_list_id: "#{l['id']}")
- end
+i = 0
+while i < num_items
+ 	l.todo_items.create(title: "#{todoitems[:title]} #{i}", description: "#{todoitems[:description]} #{i}", due_date: "#{todoitems[:due_date]}", todo_list_id: "#{l['id']}")
+ 	i += 1
+end
 
 p = User.create!({username: "Trump", password_digest: "pword"}) 
 p.create_profile!({first_name: "Donald", last_name: "Trump", birth_year: 1946, gender: "male"})
 l = TodoList.create!(list_name: "test", list_due_date: Date.today+1.year, user_id: "#{p['id']}")
-array_of_todoitems.each do |item|
- 	l.todo_items.create(title: item[:title], description: item[:description], due_date: Date.today+1.year, todo_list_id: "#{l['id']}")
- end
+i = 0
+while i < num_items
+ 	l.todo_items.create(title: "#{todoitems[:title]} #{i}", description: "#{todoitems[:description]} #{i}", due_date: "#{todoitems[:due_date]}", todo_list_id: "#{l['id']}")
+ 	i += 1
+end
 
 p = User.create!({username: "Carson", password_digest: "1234"})
 p.create_profile!({first_name: "Ben", last_name: "Carson", birth_year: 1951, gender: "male"})
 l = TodoList.create!(list_name: "test", list_due_date: Date.today+1.year, user_id: "#{p['id']}")
-array_of_todoitems.each do |item|
- 	l.todo_items.create(title: item[:title], description: item[:description], due_date: Date.today+1.year, todo_list_id: "#{l['id']}")
- end
+i = 0
+while i < num_items
+ 	l.todo_items.create(title: "#{todoitems[:title]} #{i}", description: "#{todoitems[:description]} #{i}", due_date: "#{todoitems[:due_date]}", todo_list_id: "#{l['id']}")
+ 	i += 1
+end
 
 p = User.create!({username: "Clinton", password_digest: "password"})
 p.create_profile!({first_name: "Hillary", last_name: "Clinton", birth_year: 1947, gender: "female"})	
 l = TodoList.create!(list_name: "test", list_due_date: Date.today+1.year, user_id: "#{p['id']}")
-array_of_todoitems.each do |item|
- 	l.todo_items.create(title: item[:title], description: item[:description], due_date: Date.today+1.year, todo_list_id: "#{l['id']}")
- end	
+i = 0
+while i < num_items
+ 	l.todo_items.create(title: "#{todoitems[:title]} #{i}", description: "#{todoitems[:description]} #{i}", due_date: "#{todoitems[:due_date]}", todo_list_id: "#{l['id']}")
+ 	i += 1
+end
 	
 	
 	
